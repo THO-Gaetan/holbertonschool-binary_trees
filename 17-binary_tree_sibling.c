d70face024d6f75b01678c6ad99feeca78a9124c
THO-Gaetan <9771@holbertonstudents.com>
2024-12-10 13:38:17 +0100

    first try for binary sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (node->parent->right);
 	}
-	else
+	else if (node == node->parent->left)
 		return (node->parent->left);
 	return (NULL);
 }
