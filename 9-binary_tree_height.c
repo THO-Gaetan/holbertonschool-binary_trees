1e321e06eaaa4c7a12d80455d074ec1ee2785bc3
THO-Gaetan <9771@holbertonstudents.com>
2024-12-10 13:48:21 +0100

    fixing wrong incrementation of height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,9 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t n = 0;
-
 	if (tree == NULL)
 		return (0);
 	if (tree->right == NULL)
-		return (n);
-	n += binary_tree_height(tree->right);
-	return (1 + n);
+		return (0);
+	return (1 + binary_tree_height(tree->right));
 }
