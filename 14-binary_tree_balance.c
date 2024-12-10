fbbb28b6b5c3bf39fccc7522e5a18485d029bd89
THO-Gaetan <9771@holbertonstudents.com>
2024-12-10 14:33:17 +0100

    trying to fixe binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,8 +32,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
