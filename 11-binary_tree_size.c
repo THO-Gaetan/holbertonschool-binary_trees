95141722871b3c1ae64f57f7f86f34217df3d1e9
THO-Gaetan <9771@holbertonstudents.com>
2024-12-10 11:36:25 +0100

    first try for the binary size function

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,4 +1,12 @@
+#include "binary_trees.h"
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	
+	size_t n = 0;
+
+	if (tree == NULL)
+		return (0);
+	n += binary_tree_size(tree->right);
+	n += binary_tree_size(tree->left);
+return (1 + n);
 }
